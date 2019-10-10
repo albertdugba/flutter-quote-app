25bfca681c362339c2d887292286e8586474633f
Albert Dugba <albert.dugba@gmail.com>
2019-10-10 12:26:52 +0000

    ootstrapped

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,66 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
 
 void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+  return runApp(Home());
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
-}
-
-class _DicePageState extends State<DicePage> {
-  void changeDiceFaces() {
-    setState(() {
-      leftDiceNumber = Random().nextInt(6) + 1;
-      rightDiceNumber = Random().nextInt(6) + 1;
-    });
-  }
-
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: changeDiceFaces,
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-            ),
+    return Container(
+      child: MaterialApp(
+          home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'JavaScript',
+            style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey),
           ),
-          Expanded(
-            child: FlatButton(
-              onPressed: changeDiceFaces,
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice$rightDiceNumber.png'),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+        ),
+        body: Row(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: Image.network('https://picsum.photos/500?image=5'),
             ),
-          )
-        ],
-      ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.cyan,
+                child: Text('1'),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.pinkAccent,
+                child: Text('2'),
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                color: Colors.amber,
+                child: Text('3'),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.teal,
+          onPressed: null,
+          child: Text(
+            'Click',
+          ),
+        ),
+      )),
     );
   }
 }
