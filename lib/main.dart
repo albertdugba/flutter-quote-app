5e86b8aed1871a21295df719f2ea510f547dc007
Albert Dugba <albert.dugba@gmail.com>
2019-10-11 13:53:40 +0000

    quote app

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,43 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
+import 'quote_card.dart';
 
-void main() {
-  return runApp(Home());
+void main() => runApp(MaterialApp(
+      home: QuoteList(),
+    ));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
 }
 
-class Home extends StatelessWidget {
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(author: 'Myles Munroe', text: 'Leaders are readers'),
+    Quote(author: 'Henry Ford', text: 'Consistency is very key'),
+    Quote(author: 'Brian Holt', text: ' People and experience is important'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: MaterialApp(
-          home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'JavaScript',
-            style: TextStyle(
-                fontSize: 25.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.grey),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.purple,
-        ),
-        body: Row(
-          children: <Widget>[
-            Expanded(
-              flex: 3,
-              child: Image.network('https://picsum.photos/500?image=5'),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.cyan,
-                child: Text('1'),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.pinkAccent,
-                child: Text('2'),
-              ),
-            ),
-            Expanded(
-              flex: 3,
-              child: Container(
-                padding: EdgeInsets.all(30.0),
-                color: Colors.amber,
-                child: Text('3'),
-              ),
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.teal,
-          onPressed: null,
-          child: Text(
-            'Click',
-          ),
-        ),
-      )),
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: quotes
+            .map((quote) => QuoteCard(
+                quote: quote,
+                delete: () {
+                  setState(() {
+                    quotes.remove(quote);
+                  });
+                }))
+            .toList(),
+      ),
     );
   }
 }
